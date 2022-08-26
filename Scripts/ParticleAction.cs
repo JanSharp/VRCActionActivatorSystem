78c017ce9efb835cc39b748cc491aafda3f57ee8
JanSharp <steinmeister36@gmail.com>
2022-08-26 20:00:54 +0200

    Add several editor utilities to ParticleAction

--- Scripts/ParticleAction.cs
+++ Scripts/ParticleAction.cs
@@ -6,6 +6,7 @@ using VRC.Udon;
 using UnityEditor;
 using UdonSharpEditor;
 using System.Reflection;
+using System.Linq;
 #endif
 
 namespace JanSharp
@@ -16,13 +17,14 @@ namespace JanSharp
         , IOnBuildCallback, IAction
     #endif
     {
-        [SerializeField] private ParticleSystem particle;
+        public ParticleSystem[] particles;
         [SerializeField] private UdonSharpBehaviour activator;
         [HideInInspector] public int listenerType;
 
         public void OnEvent()
         {
-            particle.Play();
+            foreach (var particle in particles)
+                particle.Play();
         }
 
         #if UNITY_EDITOR && !COMPILER_UDONSHARP
@@ -40,6 +42,34 @@ namespace JanSharp
 
     #if UNITY_EDITOR && !COMPILER_UDONSHARP
     [CustomEditor(typeof(ParticleAction))]
-    public class ParticleActionEditor : ActionEditorBase { }
+    public class ParticleActionEditor : ActionEditorBase
+    {
+        public override void OnInspectorGUI()
+        {
+            ParticleAction targetAction = this.target as ParticleAction;
+            base.OnInspectorGUI();
+            if (GUILayout.Button(new GUIContent("Find all child particle systems")))
+            {
+                targetAction.particles = targetAction.GetComponentsInChildren<ParticleSystem>();
+                targetAction.ApplyProxyModifications();
+                EditorUtility.SetDirty(targetAction);
+            }
+            if (targetAction.particles.Any(p => p == null) && GUILayout.Button(new GUIContent("Remove null particles")))
+            {
+                targetAction.particles = targetAction.particles.Where(p => p != null).ToArray();
+                targetAction.ApplyProxyModifications();
+                EditorUtility.SetDirty(targetAction);
+            }
+            if (targetAction.particles.Any(p => p != null && p.main.playOnAwake) && GUILayout.Button(new GUIContent("Disable PlayOnAwake")))
+            {
+                foreach (var particle in targetAction.particles.Where(p => p.main.playOnAwake))
+                {
+                    var main = particle.main;
+                    main.playOnAwake = false;
+                    EditorUtility.SetDirty(particle);
+                }
+            }
+        }
+    }
     #endif
 }
