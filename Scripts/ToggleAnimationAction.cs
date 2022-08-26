7d0633edb06d020727bc15a141a3547f75b638ca
JanSharp <steinmeister36@gmail.com>
2022-08-26 20:45:00 +0200

    Add editor utilities to ToggleAnimationAction

--- Scripts/ToggleAnimationAction.cs
+++ Scripts/ToggleAnimationAction.cs
@@ -6,6 +6,7 @@ using VRC.Udon;
 using UnityEditor;
 using UdonSharpEditor;
 using System.Reflection;
+using System.Linq;
 #endif
 
 namespace JanSharp
@@ -16,8 +17,8 @@ namespace JanSharp
         , IOnBuildCallback
     #endif
     {
-        [SerializeField] private Animator animator;
-        [SerializeField] private string boolParameterName = "state";
+        public Animator animator;
+        public string boolParameterName = "state";
         [SerializeField] private UdonSharpBehaviour activator;
 
         public void OnEvent()
@@ -38,4 +39,46 @@ namespace JanSharp
         }
         #endif
     }
+
+    #if UNITY_EDITOR && !COMPILER_UDONSHARP
+    [CustomEditor(typeof(ToggleAnimationAction))]
+    public class ToggleAnimationActionEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            ToggleAnimationAction targetAction = this.target as ToggleAnimationAction;
+            if (UdonSharpGUI.DrawDefaultUdonSharpBehaviourHeader(targetAction))
+                return;
+            EditorGUILayout.Space();
+            base.OnInspectorGUI(); // draws public/serializable fields
+
+            if (targetAction.animator == null
+                && targetAction.GetComponent<Animator>() != null
+                && GUILayout.Button(new GUIContent("Set Animator to this")))
+            {
+                targetAction.animator = targetAction.GetComponent<Animator>();
+                targetAction.ApplyProxyModifications();
+                EditorUtility.SetDirty(targetAction);
+            }
+
+            // an attempt at validating the animator's parameters. This now says it doesn't find the parameter even though it exists. Weird.
+            // if (targetAction.animator != null)
+            // {
+            //     var param = targetAction.animator.parameters.FirstOrDefault(p => p.name == targetAction.boolParameterName);
+            //     if (param == null)
+            //     {
+            //         EditorGUILayout.Space();
+            //         ///cSpell:ignore erroricon
+            //         // GUILayout.Label(EditorGUIUtility.TrIconContent("console.erroricon", $"The referenced animator does not have a '{targetAction.boolParameterName}' parameter."));
+            //         GUILayout.Label(new GUIContent($"The referenced animator does not have a '{targetAction.boolParameterName}' parameter."));
+            //     }
+            //     else if (param.type != AnimatorControllerParameterType.Bool)
+            //     {
+            //         EditorGUILayout.Space();
+            //         GUILayout.Label(new GUIContent($"The referenced animator's '{targetAction.boolParameterName}' parameter must be a bool parameter."));
+            //     }
+            // }
+        }
+    }
+    #endif
 }
