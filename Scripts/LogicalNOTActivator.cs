563e8a382329cdfc243ed8367552b532bc2f69bc
JanSharp <steinmeister36@gmail.com>
2022-08-26 04:15:42 +0200

    Add OR XOR and NOT activators

---
+++ Scripts/LogicalNOTActivator.cs
@@ -0,0 +1,86 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common.Interfaces;
+#if UNITY_EDITOR && !COMPILER_UDONSHARP
+using UnityEditor;
+using UdonSharpEditor;
+#endif
+
+namespace JanSharp
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class LogicalNOTActivator : UdonSharpBehaviour
+    #if UNITY_EDITOR && !COMPILER_UDONSHARP
+        , IOnBuildCallbackV2
+    #endif
+    {
+        [SerializeField] [HideInInspector] private UdonSharpBehaviour[] onActivateListeners;
+        [SerializeField] [HideInInspector] private UdonSharpBehaviour[] onDeactivateListeners;
+        [SerializeField] [HideInInspector] private UdonSharpBehaviour[] onStateChangedListeners;
+        [SerializeField] [HideInInspector] private string[] onActivateListenerEventNames;
+        [SerializeField] [HideInInspector] private string[] onDeactivateListenerEventNames;
+        [SerializeField] [HideInInspector] private string[] onStateChangedListenerEventNames;
+
+        private bool state;
+        private bool State
+        {
+            get => state;
+            set
+            {
+                if (value == state)
+                    return;
+                state = value;
+                if (value)
+                    Send(onActivateListeners, onActivateListenerEventNames);
+                else
+                    Send(onDeactivateListeners, onDeactivateListenerEventNames);
+                Send(onStateChangedListeners, onStateChangedListenerEventNames);
+            }
+        }
+
+        private void Send(UdonSharpBehaviour[] listeners, string[] listenerEventNames)
+        {
+            for (int i = 0; i < listeners.Length; i++)
+                listeners[i].SendCustomEvent(listenerEventNames[i]);
+        }
+
+        public UdonSharpBehaviour inputActivator;
+
+        public void OnEvent()
+        {
+            State = !(bool)inputActivator.GetProgramVariable("state");
+        }
+
+        #if UNITY_EDITOR && !COMPILER_UDONSHARP
+        [InitializeOnLoad]
+        public static class OnBuildRegister
+        {
+            static OnBuildRegister()
+            {
+                OnBuildUtil.RegisterTypeV2<LogicalNOTActivator>(order: 0);
+                OnBuildUtil.RegisterTypeV2<LogicalNOTActivator>(order: 1);
+            }
+        }
+        bool IOnBuildCallbackV2.OnBuild(int order)
+        {
+            if (order == 0)
+            {
+                onActivateListeners = new UdonSharpBehaviour[0];
+                onDeactivateListeners = new UdonSharpBehaviour[0];
+                onStateChangedListeners = new UdonSharpBehaviour[0];
+                onActivateListenerEventNames = new string[0];
+                onDeactivateListenerEventNames = new string[0];
+                onStateChangedListenerEventNames = new string[0];
+                this.ApplyProxyModifications();
+            }
+            else
+            {
+                ActivatorEditorUtil.AddActivatorToListeners(inputActivator, ActivatorEditorUtil.ListenerEventType.OnStateChanged, this);
+            }
+            return true;
+        }
+        #endif
+    }
+}
---
+++ Scripts/LogicalXORActivator.cs
@@ -0,0 +1,100 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common.Interfaces;
+#if UNITY_EDITOR && !COMPILER_UDONSHARP
+using UnityEditor;
+using UdonSharpEditor;
+#endif
+
+namespace JanSharp
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class LogicalXORActivator : UdonSharpBehaviour
+    #if UNITY_EDITOR && !COMPILER_UDONSHARP
+        , IOnBuildCallbackV2
+    #endif
+    {
+        [SerializeField] [HideInInspector] private UdonSharpBehaviour[] onActivateListeners;
+        [SerializeField] [HideInInspector] private UdonSharpBehaviour[] onDeactivateListeners;
+        [SerializeField] [HideInInspector] private UdonSharpBehaviour[] onStateChangedListeners;
+        [SerializeField] [HideInInspector] private string[] onActivateListenerEventNames;
+        [SerializeField] [HideInInspector] private string[] onDeactivateListenerEventNames;
+        [SerializeField] [HideInInspector] private string[] onStateChangedListenerEventNames;
+
+        private bool state;
+        private bool State
+        {
+            get => state;
+            set
+            {
+                if (value == state)
+                    return;
+                state = value;
+                if (value)
+                    Send(onActivateListeners, onActivateListenerEventNames);
+                else
+                    Send(onDeactivateListeners, onDeactivateListenerEventNames);
+                Send(onStateChangedListeners, onStateChangedListenerEventNames);
+            }
+        }
+
+        private void Send(UdonSharpBehaviour[] listeners, string[] listenerEventNames)
+        {
+            for (int i = 0; i < listeners.Length; i++)
+                listeners[i].SendCustomEvent(listenerEventNames[i]);
+        }
+
+        public UdonSharpBehaviour[] inputActivators;
+
+        public void OnEvent()
+        {
+            bool active = false;
+            foreach (var activator in inputActivators)
+            {
+                if ((bool)activator.GetProgramVariable("state"))
+                {
+                    if (active)
+                    {
+                        State = false;
+                        return;
+                    }
+                    active = true;
+                }
+            }
+            State = active;
+        }
+
+        #if UNITY_EDITOR && !COMPILER_UDONSHARP
+        [InitializeOnLoad]
+        public static class OnBuildRegister
+        {
+            static OnBuildRegister()
+            {
+                OnBuildUtil.RegisterTypeV2<LogicalXORActivator>(order: 0);
+                OnBuildUtil.RegisterTypeV2<LogicalXORActivator>(order: 1);
+            }
+        }
+        bool IOnBuildCallbackV2.OnBuild(int order)
+        {
+            if (order == 0)
+            {
+                onActivateListeners = new UdonSharpBehaviour[0];
+                onDeactivateListeners = new UdonSharpBehaviour[0];
+                onStateChangedListeners = new UdonSharpBehaviour[0];
+                onActivateListenerEventNames = new string[0];
+                onDeactivateListenerEventNames = new string[0];
+                onStateChangedListenerEventNames = new string[0];
+                this.ApplyProxyModifications();
+            }
+            else
+            {
+                foreach (var activator in inputActivators)
+                    ActivatorEditorUtil.AddActivatorToListeners(activator, ActivatorEditorUtil.ListenerEventType.OnStateChanged, this);
+            }
+            return true;
+        }
+        #endif
+    }
+}
