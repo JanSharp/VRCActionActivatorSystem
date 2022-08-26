e6bc8fe2034e1a1ccef231411ec88cf5963eb758
JanSharp <steinmeister36@gmail.com>
2022-08-26 22:59:14 +0200

    Fix compile error when publishing

--- Scripts/ActivatorEditorUtil.cs
+++ Scripts/ActivatorEditorUtil.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#if UNITY_EDITOR
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -103,3 +104,4 @@ namespace JanSharp
         }
     }
 }
+#endif
