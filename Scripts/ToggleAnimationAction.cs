39fbd60f6b788f37bb7f6988f98e22d0ca04b260
JanSharp <steinmeister36@gmail.com>
2022-08-26 18:35:24 +0200

    Add ToggleAnimationAction

---
+++ Scripts/ToggleAnimationAction.cs
@@ -0,0 +1,40 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+#if UNITY_EDITOR && !COMPILER_UDONSHARP
+using UnityEditor;
+using UdonSharpEditor;
+using System.Reflection;
+#endif
+
+namespace JanSharp
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class ToggleAnimationAction : UdonSharpBehaviour
+    #if UNITY_EDITOR && !COMPILER_UDONSHARP
+        , IOnBuildCallback
+    #endif
+    {
+        [SerializeField] private Animator animator;
+        [SerializeField] private UdonSharpBehaviour activator;
+
+        public void OnEvent()
+        {
+            animator.SetBool("state", (bool)activator.GetProgramVariable("state"));
+        }
+
+        #if UNITY_EDITOR && !COMPILER_UDONSHARP
+        [InitializeOnLoad]
+        public static class OnBuildRegister
+        {
+            static OnBuildRegister() => OnBuildUtil.RegisterType<ToggleAnimationAction>(order: 1);
+        }
+        bool IOnBuildCallback.OnBuild()
+        {
+            ActivatorEditorUtil.AddActivatorToListeners(activator, ActivatorEditorUtil.ListenerEventType.OnStateChanged, this);
+            return true;
+        }
+        #endif
+    }
+}
